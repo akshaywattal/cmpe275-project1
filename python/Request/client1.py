6b1b0e75c3f45d0f3af02fe8057832f38aeff7d6
oldmonk7 <shashankgarg7@gmail.com>
2014-04-13 02:02:48 -0700

    fine-tune python client

--- python/Request/client1.py
+++ python/Request/client1.py
@@ -34,13 +34,19 @@ def buildGetCourseDescription():
     r.header.originator = "Python Client"
     r.header.routing_id = comm_pb2.Header.JOBS
     r.body.job_op.action = comm_pb2.JobOperation.ADDJOB
-    r.body.job_op.data.name_space = "1"
-    r.body.job_op.data.owner_id = 2
-    r.body.job_op.data.job_id = "3"
-    r.body.job_op.data.status = comm_pb2.JobDesc.JOBRUNNING
-    r.body.job_op.data.options.node_type = comm_pb2.NameValueSet.NODE
-    r.body.job_op.data.options.name = "Machine Learning-2"
-    r.body.job_op.data.options.value = "CMPE275/CS101"
+    r.body.job_op.job_id = "C-1234"
+    r.body.job_op.data.name_space = "getdescription"
+    r.body.job_op.data.owner_id = 1234
+    r.body.job_op.data.job_id = "C-1234"
+    r.body.job_op.data.status = comm_pb2.JobDesc.JOBRECEIVED
+    r.body.job_op.data.options.node_type = comm_pb2.NameValueSet.VALUE
+    nv = r.body.job_op.data.options.node.add()
+    nv.node_type = comm_pb2.NameValueSet.VALUE
+    nv.name = "Machine Learning-2"
+    nv.value = "CMPE275/CS101"
+    #r.body.job_op.data.options.node.node_type = comm_pb2.NameValueSet.VALUE
+    #r.body.job_op.data.options.name = "Machine Learning-2"
+    #r.body.job_op.data.options.value = "CMPE275/CS101"
     m = r.SerializeToString()
     return m
 
@@ -48,6 +54,7 @@ def getCourseDescriptionResponse(Socket):
     while True:
         data = Socket.recv(1024)
         r = comm_pb2.Request()
+        print(data)
         r.ParseFromString(data)
         print("Recieved course name : "+(r.body.job_op.data.options.name))
         print("Recieved course value : "+(r.body.job_op.data.options.value))
@@ -56,6 +63,35 @@ def getCourseDescriptionResponse(Socket):
             return
         Socket.sendall(response)
         Socket.close()
+
+def buildListAllCourses():
+    r = comm_pb2.Request()
+    r.header.originator = "client-1"
+    r.header.routing_id = comm_pb2.Header.JOBS
+    r.body.job_op.action = comm_pb2.JobOperation.ADDJOB
+    r.body.job_op.job_id = "C-1234"
+    r.body.job_op.data.name_space = "listcourses"
+    r.body.job_op.data.owner_id = 1234
+    r.body.job_op.data.job_id = "C-1234"
+    r.body.job_op.data.status = comm_pb2.JobDesc.JOBRECEIVED
+    #r.body.job_op.data.options.node_type = comm_pb2.NameValueSet.NODE
+    m = r.SerializeToString()
+    return m
+
+def getListCoursesResponse(s):
+    while True:
+        data = s.recv(1024)
+        r = comm_pb2.Request()
+        print(data)
+        r.ParseFromString(data)
+        no_courses = len(r.body.job_op.data.options.node)
+        for i in range(0,no_courses):
+            print(r.body.job_op.data.options.node[i].value)
+        response = raw_input("Reply: ")
+        if response == "exit":
+            return
+        s.sendall(response)
+        s.close()
 
 def buildCourse(courseID, courseName, courseDescription):
 	r = comm_pb2.Request()
@@ -110,6 +146,7 @@ def getUserResponse(s):
         print(data)
         r = comm_pb2.Request()
         r.ParseFromString(data)
+        print(r.header.reply_msg)
         print("Recieved user ID : "+(r.body.space_op.u_id.user_id))
         print("Recieved user name : "+(r.body.space_op.u_id.user_name))
         response = raw_input("Reply: ")
@@ -150,5 +187,10 @@ def createSocket():
         msg = buildGetCourseDescription()
         sendMessage(s, struct.pack('>L',len(msg)), msg)
         getCourseDescriptionResponse(s)
+    elif msgType == "D":
+        msg = buildListAllCourses()
+        sendMessage(s, struct.pack('>L',len(msg)), msg)
+        getListCoursesResponse(s)
+
 
 createSocket()
